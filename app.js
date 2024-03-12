7ecb06f769e3d2d19ca205bf0699aa95b8ad49b6
Gojo-fx <johlonvi@gmail.com>
2024-03-11 21:56:22 -0500

    Feat Server added

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send("Hello, world!");
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+});
